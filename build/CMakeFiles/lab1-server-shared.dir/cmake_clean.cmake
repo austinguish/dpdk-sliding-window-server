0e9f05448cb8fb35399620c873553aebbdc75ffb
jiangyw <jiangyw@localhost>
2024-09-16 15:23:18 -0600

    init

---
+++ build/CMakeFiles/lab1-server-shared.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1-server-shared.dir/lab1-server.c.o"
+  "lab1-server-shared"
+  "lab1-server-shared.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1-server-shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/lab1-server-static.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1-server-static.dir/lab1-server.c.o"
+  "lab1-server-static"
+  "lab1-server-static.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1-server-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
