c5406a8cafb402441ad036f10af50ee18443c0e4
jiangyw <jiangyw@localhost>
2024-09-30 21:20:46 -0600

    restore the client

--- cmake-build-debug/CMakeFiles/lab1-server-shared.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/lab1-server-shared.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/tianyi/Server/lab1-server.c" "CMakeFiles/lab1-server-shared.dir/lab1-server.c.o" "gcc" "CMakeFiles/lab1-server-shared.dir/lab1-server.c.o.d"
+  "/mydata/Server/lab1-server.c" "CMakeFiles/lab1-server-shared.dir/lab1-server.c.o" "gcc" "CMakeFiles/lab1-server-shared.dir/lab1-server.c.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- cmake-build-debug/CMakeFiles/lab1-server-static.dir/lab1-server.c.o.d
+++ cmake-build-debug/CMakeFiles/lab1-server-static.dir/lab1-server.c.o.d
@@ -1,13 +1,13 @@
-CMakeFiles/lab1-client-shared.dir/lab1-client.c.o: \
- /mydata/Server/lab1-client.c /usr/include/stdc-predef.h \
- /mydata/Server/udp_header.h /usr/local/include/rte_udp.h \
- /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
- /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
- /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+CMakeFiles/lab1-server-static.dir/lab1-server.c.o: \
+ /mydata/Server/lab1-server.c /usr/include/stdc-predef.h \
+ /usr/include/inttypes.h /usr/include/features.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
  /usr/include/x86_64-linux-gnu/bits/long-double.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
  /usr/include/x86_64-linux-gnu/bits/types.h \
  /usr/include/x86_64-linux-gnu/bits/timesize.h \
  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
@@ -15,29 +15,6 @@ CMakeFiles/lab1-client-shared.dir/lab1-client.c.o: \
  /usr/include/x86_64-linux-gnu/bits/wchar.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
- /usr/local/include/rte_byteorder.h /usr/local/include/rte_common.h \
- /usr/include/assert.h /usr/lib/gcc/x86_64-linux-gnu/9/include/limits.h \
- /usr/lib/gcc/x86_64-linux-gnu/9/include/syslimits.h \
- /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
- /usr/include/x86_64-linux-gnu/bits/local_lim.h \
- /usr/include/linux/limits.h \
- /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
- /usr/lib/gcc/x86_64-linux-gnu/9/include/stdalign.h \
- /usr/local/include/rte_config.h /usr/local/include/rte_build_config.h \
- /usr/local/include/rte_os.h /usr/include/sched.h \
- /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
- /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
- /usr/include/x86_64-linux-gnu/bits/endian.h \
- /usr/include/x86_64-linux-gnu/bits/endianness.h \
- /usr/include/x86_64-linux-gnu/bits/sched.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_sched_param.h \
- /usr/include/x86_64-linux-gnu/bits/cpu-set.h \
- /usr/include/x86_64-linux-gnu/sys/queue.h \
- /usr/local/include/generic/rte_byteorder.h /usr/include/endian.h \
- /usr/include/x86_64-linux-gnu/bits/byteswap.h \
- /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
- /usr/local/include/rte_byteorder_64.h /usr/include/inttypes.h \
  /usr/local/include/rte_cycles.h \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/x86intrin.h \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/ia32intrin.h \
@@ -45,19 +22,26 @@ CMakeFiles/lab1-client-shared.dir/lab1-client.c.o: \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/mmintrin.h \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/xmmintrin.h \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/mm_malloc.h \
- /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/stdlib.h /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
  /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
  /usr/include/x86_64-linux-gnu/bits/floatn.h \
  /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
  /usr/include/x86_64-linux-gnu/sys/types.h \
  /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
  /usr/include/x86_64-linux-gnu/sys/select.h \
  /usr/include/x86_64-linux-gnu/bits/select.h \
  /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
  /usr/include/x86_64-linux-gnu/bits/select2.h \
  /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
  /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
@@ -135,7 +119,7 @@ CMakeFiles/lab1-client-shared.dir/lab1-client.c.o: \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/mwaitxintrin.h \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/clzerointrin.h \
  /usr/local/include/generic/rte_cycles.h /usr/local/include/rte_debug.h \
- /usr/local/include/rte_log.h /usr/include/stdio.h \
+ /usr/local/include/rte_log.h /usr/include/assert.h /usr/include/stdio.h \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h \
  /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
@@ -148,6 +132,20 @@ CMakeFiles/lab1-client-shared.dir/lab1-client.c.o: \
  /usr/include/x86_64-linux-gnu/bits/stdio.h \
  /usr/include/x86_64-linux-gnu/bits/stdio2.h \
  /usr/lib/gcc/x86_64-linux-gnu/9/include/stdbool.h \
+ /usr/local/include/rte_common.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdalign.h \
+ /usr/local/include/rte_config.h /usr/local/include/rte_build_config.h \
+ /usr/local/include/rte_os.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_sched_param.h \
+ /usr/include/x86_64-linux-gnu/bits/cpu-set.h \
+ /usr/include/x86_64-linux-gnu/sys/queue.h \
  /usr/local/include/rte_branch_prediction.h \
  /usr/local/include/rte_atomic.h /usr/local/include/generic/rte_atomic.h \
  /usr/local/include/rte_stdatomic.h /usr/local/include/rte_atomic_64.h \
@@ -206,9 +204,11 @@ CMakeFiles/lab1-client-shared.dir/lab1-client.c.o: \
  /usr/local/include/rte_prefetch.h \
  /usr/local/include/generic/rte_prefetch.h \
  /usr/local/include/rte_mbuf_ptype.h /usr/local/include/rte_mbuf_core.h \
- /usr/local/include/rte_flow.h /usr/local/include/rte_arp.h \
- /usr/local/include/rte_icmp.h /usr/local/include/rte_ip.h \
- /usr/include/x86_64-linux-gnu/sys/socket.h \
+ /usr/local/include/rte_byteorder.h \
+ /usr/local/include/generic/rte_byteorder.h \
+ /usr/local/include/rte_byteorder_64.h /usr/local/include/rte_flow.h \
+ /usr/local/include/rte_arp.h /usr/local/include/rte_icmp.h \
+ /usr/local/include/rte_ip.h /usr/include/x86_64-linux-gnu/sys/socket.h \
  /usr/include/x86_64-linux-gnu/bits/types/struct_iovec.h \
  /usr/include/x86_64-linux-gnu/bits/socket.h \
  /usr/include/x86_64-linux-gnu/bits/socket_type.h \
@@ -228,10 +228,11 @@ CMakeFiles/lab1-client-shared.dir/lab1-client.c.o: \
  /usr/include/x86_64-linux-gnu/bits/in.h /usr/include/arpa/inet.h \
  /usr/include/netinet/ip.h /usr/include/netinet/ip6.h \
  /usr/local/include/rte_sctp.h /usr/local/include/rte_tcp.h \
- /usr/local/include/rte_vxlan.h /usr/local/include/rte_esp.h \
- /usr/local/include/rte_higig.h /usr/local/include/rte_ecpri.h \
- /usr/local/include/rte_mbuf_dyn.h /usr/local/include/rte_meter.h \
- /usr/local/include/rte_gtp.h /usr/local/include/rte_l2tpv2.h \
- /usr/local/include/rte_ppp.h /usr/local/include/rte_gre.h \
- /usr/local/include/rte_macsec.h /usr/local/include/rte_ib.h \
- /usr/local/include/rte_ethdev.h /usr/local/include/rte_ethdev_core.h
+ /usr/local/include/rte_udp.h /usr/local/include/rte_vxlan.h \
+ /usr/local/include/rte_esp.h /usr/local/include/rte_higig.h \
+ /usr/local/include/rte_ecpri.h /usr/local/include/rte_mbuf_dyn.h \
+ /usr/local/include/rte_meter.h /usr/local/include/rte_gtp.h \
+ /usr/local/include/rte_l2tpv2.h /usr/local/include/rte_ppp.h \
+ /usr/local/include/rte_gre.h /usr/local/include/rte_macsec.h \
+ /usr/local/include/rte_ib.h /usr/local/include/rte_ethdev.h \
+ /usr/local/include/rte_ethdev_core.h /mydata/Server/udp_header.h
